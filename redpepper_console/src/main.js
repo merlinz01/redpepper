9cba0288210a30e9ec3970bfd59360128df6358e
merlinz01 <158784988+merlinz01@users.noreply.github.com>
2024-05-28 13:44:20 -0400

    use hash history mode in console path router

--- redpepper_console/src/main.js
+++ redpepper_console/src/main.js
@@ -1,7 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import App from './App.vue'
 
@@ -26,7 +26,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 createApp(App).use(router).mount('body')
