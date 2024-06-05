a4964287ed79ea5b39a52f56b279028e5a8f60f9
merlinz01 <158784988+merlinz01@users.noreply.github.com>
2024-06-04 20:03:00 -0400

    better Debian-like checking for package state

--- redpepper/states/package.py
+++ redpepper/states/package.py
@@ -6,7 +6,7 @@ if sys.platform == "linux":
     distro = require_python_package("distro")
     d = distro.id()
     like = distro.like()
-    if "debian" in like:
+    if d == "debian" or "debian" in like:
         from redpepper.states.apt import *
     else:
         raise ImportError(
