254b3ba6ad4da6389ec36f418ef31ea89a0ec295
merlinz01 <158784988+merlinz01@users.noreply.github.com>
2024-06-06 15:49:50 -0400

    add systemd.Enabled operation

--- redpepper/operations/systemd.py
+++ redpepper/operations/systemd.py
@@ -23,3 +23,25 @@ class Running(Operation):
         if result.check_completed_process(p).succeeded:
             result.changed = True
         return result
+
+
+class Enabled(Operation):
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"systemd.Enabled({self.name})"
+
+    def test(self, agent):
+        cmd = ["systemctl", "is-enabled", self.name]
+        p = subprocess.run(cmd)
+        return p.returncode == 0
+
+    def run(self, agent):
+        result = Result(self)
+        cmd = ["systemctl", "enable", self.name]
+        p = subprocess.run(cmd, capture_output=True, text=True)
+        if result.check_completed_process(p).succeeded:
+            result.changed = True
+        return result
