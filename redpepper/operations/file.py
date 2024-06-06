ac2b3fba7db586d7a9016ece9d011cb0581451cd
merlinz01 <158784988+merlinz01@users.noreply.github.com>
2024-06-06 17:15:04 -0400

    refactor of file.Installed

--- redpepper/operations/file.py
+++ redpepper/operations/file.py
@@ -40,59 +40,76 @@ class Installed(Operation):
         self.mode = mode
 
     def __str__(self):
-        return f"file.Installed({self.path} from {self.source})"
+        return f'file.Installed("{self.path}" from "{self.source}")'
 
-    def test(self, agent):
-        if not os.path.isfile(self.path):
-            logger.debug("File %s does not exist", self.path)
-            return False
-        stat = os.stat(self.path)
-        if self.user is not None and self.user != stat.st_uid:
-            return False
-        if self.group is not None and self.group != stat.st_gid:
-            return False
-        if self.mode is not None and self.mode != stat.st_mode:
-            return False
-        if not self.overwrite:
-            return True
-        if self.method == "mtime":
-            ok, data = agent.request_data("file_mtime", self.source)
-            if not ok:
-                logger.error("Failed to get mtime: %s", data)
-                return False
-            try:
-                mtime = float(data)
-            except ValueError:
-                logger.error("Failed to parse mtime: %s", data)
-                return False
-            return os.path.getmtime(self.path) == mtime
-        elif self.method == "hash":
-            ok, hash = agent.request_data("file_hash", self.source)
-            if not ok:
-                logger.error("Failed to get hash: %s", hash)
-                return False
-            return self.hash_file(self.path) == hash
-        return False
-
-    def run(self, agent):
+    def ensure(self, agent):
         result = Result(self)
-        if not self.overwrite and os.path.isfile(self.path):
-            result += f"File {self.path} already exists, not changing content due to overwrite=False."
-            stat = os.stat(self.path)
+        # Open in binary writable mode without truncating the file
+        try:
+            f = open(self.path, "r+b")
+        except FileNotFoundError:
+            f = open(self.path, "wb")
+        with f:
+            stat = os.fstat(f.fileno())
             if self.mode is not None and stat.st_mode & 0o777 != self.mode:
-                os.chmod(self.path, self.mode)
+                os.fchmod(f.fileno(), self.mode)
                 result += (
                     f"Changed mode from 0{stat.st_mode & 0o777:o} to 0{self.mode:o}."
                 )
                 result.changed = True
             if self.user is not None and stat.st_uid != self.user:
-                os.chown(self.path, self.user, -1)
+                os.fchown(f.fileno(), self.user, -1)
                 result += f"Changed owner from {stat.st_uid} to {self.user}."
                 result.changed = True
             if self.group is not None and stat.st_gid != self.group:
-                os.chown(self.path, -1, self.group)
+                os.fchown(f.fileno(), -1, self.group)
                 result += f"Changed group from {stat.st_gid}to {self.group}."
                 result.changed = True
+            if self.overwrite:
+                try:
+                    needs_rewritten = self.check_needs_rewritten(agent)
+                except ValueError as e:
+                    result.exception()
+                    return result
+                if needs_rewritten:
+                    try:
+                        nwritten = self.write_file(agent, f)
+                    except Exception:
+                        result.exception()
+                    else:
+                        result += f"Wrote {nwritten} bytes to {self.path}."
+                        result.changed = True
+        return result
+
+    def check_needs_rewritten(self, agent):
+        if not self.overwrite:
+            return True
+        if self.method == "mtime":
+            ok, data = agent.request_data("file_mtime", self.source)
+            if not ok:
+                raise ValueError("Failed to get mtime: %s", data)
+            try:
+                mtime = float(data)
+            except ValueError:
+                raise ValueError(f"Invalid mtime received: {data}")
+            try:
+                existing_mtime = os.path.getmtime(self.path)
+            except FileNotFoundError:
+                existing_mtime = None
+            logger.debug("Mtime of %s: %s vs. %s", self.path, existing_mtime, mtime)
+            return existing_mtime == mtime
+        elif self.method == "hash":
+            ok, hash = agent.request_data("file_hash", self.source)
+            if not ok:
+                raise ValueError("Failed to get hash: %s", hash)
+            existing_hash = self.hash_file(self.path)
+            logger.debug("Hash of %s: %s vs. %s", self.path, existing_hash, hash)
+            return existing_hash == hash
+        return False
+
+    def write_file(self, agent, f: io.BufferedIOBase):
+        # Retrieve the entire file into the buffer first so that we don't
+        # leave the file in an inconsistent state if the connection is lost
         contents = io.BytesIO()
         while True:
             parameters = {
@@ -102,33 +119,24 @@ class Installed(Operation):
             }
             ok, data = agent.request_data("file_content", json.dumps(parameters))
             if not ok:
-                result.fail("Failed to download file contents: " + data)
-                return result
+                raise ValueError(f"Failed to get file content: {data}")
             if not data:
                 break
             contents.write(data)
-        with open(self.path, "wb") as f:
-            stat = os.fstat(f.fileno())
-            if self.mode is not None and stat.st_mode & 0o777 != self.mode:
-                os.fchmod(f.fileno(), self.mode)
-                result += (
-                    f"Changed mode from 0{stat.st_mode & 0o777:o} to 0{self.mode:o}."
-                )
-            if self.user is not None and stat.st_uid != self.user:
-                os.fchown(f.fileno(), self.user, -1)
-                result += f"Changed owner from {stat.st_uid} to {self.user}."
-            if self.group is not None and stat.st_gid != self.group:
-                os.fchown(f.fileno(), -1, self.group)
-                result += f"Changed group from {stat.st_gid}to {self.group}."
-            f.write(contents.getvalue())
-        result += f"File {self.path} written with {contents.tell()} bytes."
-        result.changed = True
+        f.write(contents.getbuffer())
+        f.truncate()
+        f.flush()
+        os.fsync(f.fileno())
+        nwritten = contents.tell()
         contents.close()
-        return result
+        return nwritten
 
     def hash_file(self, path):
-        hash = hashlib.sha256()
-        with open(path, "rb") as f:
-            for chunk in iter(lambda: f.read(4096), b""):
-                hash.update(chunk)
-        return hash.digest()
+        try:
+            hash = hashlib.sha256()
+            with open(path, "rb") as f:
+                for chunk in iter(lambda: f.read(4096), b""):
+                    hash.update(chunk)
+            return hash.digest()
+        except (FileNotFoundError, IsADirectoryError):
+            return None
