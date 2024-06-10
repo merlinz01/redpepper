e3ff4f0a1460d761e260646af4c8040275c533b5
merlinz01 <158784988+merlinz01@users.noreply.github.com>
2024-06-10 15:12:40 -0400

    add sysctl.Parameter operation

---
+++ redpepper/operations/sysctl.py
@@ -0,0 +1,56 @@
+import os
+import subprocess
+import sys
+
+from redpepper.operations import Operation, Result
+
+if sys.platform != "linux":
+    raise ImportError("sysctl module only supported on Linux")
+
+SYSCTL_VERSION = int(
+    subprocess.run(
+        ["dpkg-query", "--showformat", "${Version}\n", "-W", "systemd"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    .stdout.strip()
+    .split(".")[0]
+)
+
+SYSCTL_CONF_PATH = "./etc-sysctl.d-99-redpepper.conf"
+if SYSCTL_VERSION < 207:
+    SYSCTL_CONF_PATH = "./etc-sysctl.conf"
+
+
+class Parameter(Operation):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = str(value)
+
+    def __str__(self):
+        return f"sysctl.Parameter({self.name}, {self.value})"
+
+    def test(self, agent):
+        if not os.path.exists(SYSCTL_CONF_PATH):
+            return False
+        with open(SYSCTL_CONF_PATH, "r") as f:
+            for line in f:
+                line = line.strip()
+                if line.startswith("#") or "=" not in line:
+                    continue
+                k, v = line.split("=")
+                if k.strip() == self.name and v.strip() == self.value:
+                    return True
+        return False
+
+    def run(self, agent):
+        result = Result(self)
+        with open(SYSCTL_CONF_PATH, "a") as f:
+            f.write(f"\n{self.name} = {self.value}\n")
+        result += (
+            f'Added sysctl parameter "{self.name} = {self.value}" to {SYSCTL_CONF_PATH}'
+        )
+        p = subprocess.run(["sysctl", "--system"], capture_output=True, text=True)
+        result.check_completed_process(p)
+        return result
