7e4f4c0578c2f135113231389e605e0d8728f081
merlinz01 <158784988+merlinz01@users.noreply.github.com>
2024-06-12 20:20:01 -0400

    update request operation parameter name

--- redpepper/operations/request.py
+++ redpepper/operations/request.py
@@ -2,32 +2,32 @@ from redpepper.operations import Operation, Result
 
 
 class Request(Operation):
-    def __init__(self, name, **kwargs):
-        self.name = name
+    def __init__(self, request_name, **kwargs):
+        self.request_name = request_name
         self.kwargs = kwargs
 
     def __str__(self):
-        return f"request.Request({self.name})"
+        return f"request.Request({self.request_name})"
 
     def run(self, agent):
         result = Result(self)
-        data = agent.request(self.name, **self.kwargs)
+        data = agent.request(self.request_name, **self.kwargs)
         result.succeeded = True
         result += str(data)
         return result
 
 
 class StateRequest(Operation):
-    def __init__(self, name, **kwargs):
-        self.name = name
+    def __init__(self, request_name, **kwargs):
+        self.request_name = request_name
         self.kwargs = kwargs
 
     def __str__(self):
-        return f"request.StateRequest({self.name})"
+        return f"request.StateRequest({self.request_name})"
 
     def run(self, agent):
         result = Result(self)
-        data = agent.request(self.name, **self.kwargs)
+        data = agent.request(self.request_name, **self.kwargs)
         result.succeeded = data["succeeded"]
         result.changed = data["changed"]
         result += data["output"]
