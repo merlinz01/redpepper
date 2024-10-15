c07e6635c2d60212287fa2d01931e5f7093e56d8
merlinz01 <na@notaccessible.xyz>
2024-10-15 19:24:38 -0400

    make bump_version script update changelog

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -46,11 +46,30 @@ def bump_version(part: VersionPart = VersionPart.patch, dry_run: bool = False):
     version = f"{major}.{minor}.{patch}"
     typer.echo(f'New version: "{version}"')
 
+    with open("CHANGELOG.md") as f:
+        content = f.read()
+    if content.count("## [Unreleased]") != 1:
+        typer.secho(
+            "Could not find '## [Unreleased]' in CHANGELOG.md", fg=typer.colors.RED
+        )
+        raise typer.Abort()
+    content = content.replace("## [Unreleased]", f"## [{version}]")
     if not dry_run:
+        typer.echo("Updating changelog")
+        with open("CHANGELOG.md", "w") as f:
+            f.write(content)
+    else:
+        typer.echo_via_pager(content)
+
+    if not dry_run:
+        typer.echo("Writing version file")
         with open("src/redpepper/version.py", "w") as version_file:
             version_file.write(f'__version__ = "{version}"\n')
 
-        subprocess.check_call(["git", "add", "src/redpepper/version.py"])
+    if not dry_run:
+        subprocess.check_call(
+            ["git", "add", "CHANGELOG.md", "src/redpepper/version.py"]
+        )
         subprocess.check_call(["git", "commit", "-m", f"Bump version to {version}"])
         subprocess.check_call(["git", "tag", version])
 
