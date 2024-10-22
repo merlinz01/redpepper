1663d7f3cfd57232d841b3f375780764f5d8f13c
merlinz01 <na@notaccessible.xyz>
2024-10-22 14:29:48 -0400

    Set up auto-imports for vue and vue-router

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -3,10 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import AutoImport from 'unplugin-auto-import/vite'
+import { Options } from 'unplugin-auto-import/types'
+
+const autoImports: Options = {
+  dts: 'src/auto-imports.d.ts',
+  vueTemplate: true,
+  dirs: ['src/components'],
+  imports: ['vue', 'vue-router']
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify()],
+  plugins: [vue(), vuetify(), AutoImport(autoImports)],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
